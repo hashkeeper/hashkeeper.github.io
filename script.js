0a2d0044438f34e7ac0d5ede8d1c5df70e24d0a1
hashkeeper <77951297+hashkeeper@users.noreply.github.com>
2021-01-26 02:47:51 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { VFXProvider } from 'react-vfx';
+
+//react vfx testing
+function App {
+  return (
+    <VFXProvider>
+      <VFXSpan>Hello world!</VFXSpan>
+    </VFXProvider>
+  );
+}
+
+App();
+
+//W3 Class Template
+/class Car extends React.Component {
+//  render() {
+//    return <h2>I am a {this.props.brand}!</h2>
+//  }
+//}
+
+//const myelement = <Car brand="Ford" />;
+
+//ReactDOM.render(myelement, document.getElementById('root'));

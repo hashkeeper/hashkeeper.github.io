9b65f164b8035ffde908b5dce8896917bcb4bc22
hashkeeper <77951297+hashkeeper@users.noreply.github.com>
2022-05-05 07:40:44 -0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const e = React.createElement;
-
-function App() {
-    const element = (<h1>Geez, okay</h1>);
-    ReactDOM.render(element, document.getElementById('root'));
-}
-
-
-setInterval(App, 1000);
